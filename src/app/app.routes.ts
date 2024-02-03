9785a09cf6b8792190c97b6b0843faa4b5b63e1d
pedrogpteixeira <pgpteixeira9@gmail.com>
2024-02-03 04:27:52 +0000

    component path test 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import {HomePageComponent} from "./Components/home-page/home-page.component";
 import {Error404Component} from "./Components/error-404/error-404.component";
 
 export const routes: Routes = [
-  {path: '', component: HomePageComponent},
   {path: 'home', component: HomePageComponent},
-  {path: 'error404', component: Error404Component},
+  {path: 'error-404', component: Error404Component},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: Error404Component},
 ];
