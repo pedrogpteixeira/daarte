b7bb975426bfc915c562dba5993c1cc0ce7f2fc3
pedrogpteixeira <pgpteixeira9@gmail.com>
2024-02-03 04:09:09 +0000

    new scrollbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {HomePageComponent} from "./Components/home-page/home-page.component";
 import {Error404Component} from "./Components/error-404/error-404.component";
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', component: HomePageComponent},
   {path: 'home', component: HomePageComponent},
   {path: '**', component: Error404Component}
 ];
