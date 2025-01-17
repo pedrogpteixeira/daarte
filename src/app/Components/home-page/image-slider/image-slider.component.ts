e380f11b49ff804e1836822bf5521b5eff7b4dac
Pedro Teixeira <pgpteixeira9@gmail.com>
2025-01-17 22:14:03 +0000

    image auto slider

--- src/app/Components/home-page/image-slider/image-slider.component.ts
+++ src/app/Components/home-page/image-slider/image-slider.component.ts
@@ -1,17 +1,73 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-image-slider',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './image-slider.component.html',
-  styleUrl: './image-slider.component.css'
+  styleUrls: ['./image-slider.component.css']
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit, OnDestroy {
 
-  imageUrl: string = 'assets/images/home1.png'; // Caminho da imagem inicial
+  images: string[] = [];
+  currentImage = 0;
+  imageIdForCSS = 1;
+  intervalId!: any;
 
   ngOnInit(): void {
+    this.images = [
+      'assets/images/home1.png',
+      'assets/images/home2.jpeg',
+      'assets/images/home3.jpeg',
+      'assets/images/home4.jpeg'
+    ];
+
+    this.startAutoSlide();
   }
 
+  ngOnDestroy(): void {
+    this.stopAutoSlide();
+  }
+
+  prevImage(): void {
+    if (this.currentImage > 0) {
+      this.currentImage--;
+    } else {
+      this.currentImage = this.images.length - 1;
+    }
+    this.updateImageIdForCSS();
+  }
+
+  nextImage(): void {
+    if (this.currentImage < this.images.length - 1) {
+      this.currentImage++;
+    } else {
+      this.currentImage = 0;
+    }
+    this.updateImageIdForCSS();
+  }
+
+  goToImage(image: number): void {
+    this.currentImage = image - 1;
+    this.updateImageIdForCSS();
+  }
+
+  updateImageIdForCSS(): void {
+    this.imageIdForCSS = this.currentImage + 1;
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.nextImage();
+    }, 5000);
+  }
+
+  stopAutoSlide(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
