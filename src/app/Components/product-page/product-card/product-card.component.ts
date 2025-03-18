bc9df0b59ca5e9ff30ded226fd23be38ad15011a
Pedro Teixeira <pgpteixeira9@gmail.com>
2025-03-18 16:12:03 +0000

    product page UI few updates

--- src/app/Components/product-page/product-card/product-card.component.ts
+++ src/app/Components/product-page/product-card/product-card.component.ts
@@ -1,12 +1,25 @@
 import {Component, Input} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  @Input() product: { name: string, price: string, image: string } = {name: '', price: '', image: ''};
+  @Input() product: { name: string, price: string, image: string, image2: string, goto: string } = {
+    name: '',
+    price: '',
+    image: '',
+    image2: '',
+    goto: ''
+  };
+
+  changeImage(image: any, src: string) {
+    image.src = src;
+  }
 }
--- src/app/Components/product-page/product-page.component.ts
+++ src/app/Components/product-page/product-page.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
 import {ProductCardComponent} from "./product-card/product-card.component";
-import { MatIconModule } from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-page',
@@ -33,21 +33,29 @@ export class ProductPageComponent {
       name: "Classic Running Shoes",
       price: "$99.99",
       image: "https://images.unsplash.com/photo-1605348532760-6753d2c43329?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      image2: "https://images.unsplash.com/photo-1562183241-b937e95585b6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      goto: "classic-running-shoes"
     },
     {
       name: "Sport Training Shoes",
       price: "$119.99",
       image: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+      image2: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      goto: "sport-training-shoes"
     },
     {
       name: "Trail Running Shoes",
       price: "$139.99",
       image: "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
+      image2: "https://images.unsplash.com/photo-1711466067057-d1bd10183924?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      goto: "trail-running-shoes"
     },
     {
       name: "Lightweight Runners",
       price: "$109.99",
       image: "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      image2: "https://images.unsplash.com/photo-1600269452121-4f2416e55c28?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      goto: "lightweight-runners"
     },
   ];
 
